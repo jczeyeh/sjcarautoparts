2dad808a2f99db65e0a18c847dfddf95281ce4a1
jczeyeh <imjcze0502@gmail.com>
2026-09-21 14:32:05 +0800

    Create products.js

---
+++ products.js
@@ -0,0 +1,90 @@
+const products = {
+
+    "shock-absorber": {
+
+        name: "Shock Absorber",
+
+        image: "images/shock-absorber.jpg",
+
+        description:
+            "Front and rear shock absorbers suitable for selected Proton, Perodua, Toyota and Honda models.",
+
+        brands: [
+
+            {
+                name: "KYB",
+                image: "images/brands/kyb.jpg"
+            },
+
+            {
+                name: "Monroe",
+                image: "images/brands/monroe.jpg"
+            },
+
+            {
+                name: "Sachs",
+                image: "images/brands/sachs.jpg"
+            }
+
+        ]
+
+    },
+
+
+    "brake-disc": {
+
+        name: "Brake Disc",
+
+        image: "images/brake-disc.jpg",
+
+        description:
+            "Replacement brake discs for various Malaysian and Japanese vehicles.",
+
+        brands: [
+
+            {
+                name: "Brembo",
+                image: "images/brands/brembo.jpg"
+            },
+
+            {
+                name: "TRW",
+                image: "images/brands/trw.jpg"
+            },
+
+            {
+                name: "Bendix",
+                image: "images/brands/bendix.jpg"
+            }
+
+        ]
+
+    },
+
+
+    "water-pump": {
+
+        name: "Water Pump",
+
+        image: "images/water-pump.jpg",
+
+        description:
+            "Engine cooling water pumps for selected vehicle applications.",
+
+        brands: [
+
+            {
+                name: "GMB",
+                image: "images/brands/gmb.jpg"
+            },
+
+            {
+                name: "AISIN",
+                image: "images/brands/aisin.jpg"
+            }
+
+        ]
+
+    }
+
+};
